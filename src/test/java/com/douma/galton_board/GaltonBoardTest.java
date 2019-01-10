94f2b488c3a32f8f1985c4f4ba9c21bbf3faf92f
Stefan Douma <stef@reyez.nl>
2019-01-11 00:56:07 +0100

    Update right oriented drop policy

--- src/test/java/com/douma/galton_board/GaltonBoardTest.java
+++ src/test/java/com/douma/galton_board/GaltonBoardTest.java
@@ -34,4 +34,26 @@ public class GaltonBoardTest extends TestCase
         assertEquals(0, resultTrays[0].getNumber());
         assertEquals(10, resultTrays[0].getNumberOfBullets());
     }
+
+    public void test_should_drop_bullets_and_be_in_right_most_tray() {
+        Bullet[] bullets = Bullet.listFromLength(10);
+        Tray[] trays = Tray.listFromLength(10);
+        DropPolicyInterface dropPolicy = new RightOrientedDropPolicy();
+
+        GaltonBoard galtonBoard = new GaltonBoard(trays, bullets, dropPolicy);
+
+        try
+        {
+            galtonBoard.dropBullets();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+            assert(false);
+        }
+
+        Tray[] resultTrays = galtonBoard.getTrays();
+        assertEquals(9, resultTrays[9].getNumber());
+        assertEquals(10, resultTrays[9].getNumberOfBullets());
+    }
 }
